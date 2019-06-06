1c00691e676ae04673b7031a1c0c83280a7e3dfb
jackielee <jackielee@192.168.1.3>
2019-06-06 18:20:21 +0800

    修改交易订单查询接口

--- src/main/java/com/jeancoder/trade/entry/mgr/get_orderitem.groovy
+++ src/main/java/com/jeancoder/trade/entry/mgr/get_orderitem.groovy
@@ -15,21 +15,27 @@ Result view = new Result();
 
 JCLogger Logger = LoggerSource.getLogger(this.class);
 def order_id = JC.request.param('order_id');
+def order_no = JC.request.param('order_no');
 def oc = JC.request.param('oc');
+
 if(oc.equals("1000")){
-	SimpleAjax order_item = NativeUtil.connect(SimpleAjax.class, "scm", "order/order_item_scm_list", ["order_no":order_id])
+	//SimpleAjax order_item = NativeUtil.connect(SimpleAjax.class, "scm", "order/order_item_scm_list", ["order_no":order_id])
+	SimpleAjax order_item = JC.internal.call(SimpleAjax.class, "scm", "order/order_item_scm_list", ["order_no":order_no, order_id:order_id])
 	return order_item;
 }else if(oc.equals("8000")){
-	SimpleAjax order_item = NativeUtil.connect(SimpleAjax.class, "crm", "order/order_mc_list", ["order_no":order_id])
+	//SimpleAjax order_item = NativeUtil.connect(SimpleAjax.class, "crm", "order/order_mc_list", ["order_no":order_id])
+	SimpleAjax order_item = JC.internal.call(SimpleAjax.class, "crm", "order/order_mc_list", ["order_no":order_no, order_id:order_id])
 	return order_item;
 }else if(oc.equals("8001")){
-	SimpleAjax order_item = NativeUtil.connect(SimpleAjax.class, "crm", "order/order_mc_recharge_list", ["order_no":order_id])
+	//SimpleAjax order_item = NativeUtil.connect(SimpleAjax.class, "crm", "order/order_mc_recharge_list", ["order_no":order_id])
+	SimpleAjax order_item = JC.internal.call(SimpleAjax.class, "crm", "order/order_mc_recharge_list", ["order_no":order_no, order_id:order_id])
 	return order_item;
 }else if(oc.equals("2000")){
-	Logger.info("order_id"+order_id)
-	SimpleAjax order_item = NativeUtil.connect(SimpleAjax.class, "ticketingsys", "ticketing/ticketing_choose_seat", ["order_no":order_id])
+	//SimpleAjax order_item = NativeUtil.connect(SimpleAjax.class, "ticketingsys", "ticketing/ticketing_choose_seat", ["order_no":order_id])
+	SimpleAjax order_item = JC.internal.call(SimpleAjax.class, "ticketingsys", "ticketing/ticketing_choose_seat", ["order_no":order_no, order_id:order_id])
 	return order_item;
 }else if(oc.equals("2010")){
-	SimpleAjax order_item = NativeUtil.connect(SimpleAjax.class, "ticketingsys", "ticketing/ticketing_reserve_seat", ["order_no":order_id])
+	//SimpleAjax order_item = NativeUtil.connect(SimpleAjax.class, "ticketingsys", "ticketing/ticketing_reserve_seat", ["order_no":order_id])
+	SimpleAjax order_item = JC.internal.call(SimpleAjax.class, "ticketingsys", "ticketing/ticketing_reserve_seat", ["order_no":order_no, order_id:order_id])
 	return order_item;
 }
